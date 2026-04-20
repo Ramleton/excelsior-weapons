1eb585eb3557b1e764a45dbb82afe5acd2bd8742
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-04-20 12:14:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.itemProperties.excelsiorWeaponsExploit = {
+    label: "Exploit",
+    reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.mMYvRfMylUuRO9VS"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("excelsiorWeaponsExploit");
+
+  CONFIG.DND5E.itemProperties.excelsiorWeaponsBlackpowder = {
+    label: "Blackpowder",
+    reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.BgAilvf6p3HlGPcv"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("excelsiorWeaponsBlackpowder");
+
+  CONFIG.DND5E.itemProperties.excelsiorWeaponsFocus = {
+    label: "Focus",
+    reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.713KrS202H3eBBbm"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("excelsiorWeaponsFocus");
+
+  CONFIG.DND5E.itemProperties.excelsiorWeaponsReliable = {
+    label: "Reliable",
+    reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.8PBNyesbgCgMaUTr"
+  };
+  CONFIG.DND5E.validProperties.weapon.add("excelsiorWeaponsReliable");
+
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsBackswing = {
+      label: "Excelsior Weapon - Backswing",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.UxUe6mrLdxVm4ygf"
+  };
+
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsCleave = {
+      label: "Excelsior Weapons - Cleave",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.tzWc1sQHzGHdhpzE"
+  };
+
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsDisarm = {
+      label: "Excelsior Weapons - Disarm",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.wenoGOR8gXbsfZ9C"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsGraze = {
+      label: "Excelsior Weapons - Graze",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.PTm01zd3SNA2twRd"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsNick = {
+      label: "Excelsior Weapons - Nick",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.BM2MF3O0GscCF8y3"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsParry = {
+      label: "Excelsior Weapons - Parry",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.X0ancNxdqo9t5Dpg"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsPush = {
+      label: "Excelsior Weapons - Push",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.TbyDkGmSMFKdlyVP"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsRepeating = {
+      label: "Excelsior Weapons - Repeating",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.mNchjpkMl1oTNPfE"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsSap = {
+      label: "Excelsior Weapons - Sap",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.D5sGD85gY5fLM7G7"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsSilent = {
+      label: "Excelsior Weapons - Silent",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.dgW6SZLHC3b7s1Ld"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsSlow = {
+      label: "Excelsior Weapons - Slow",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.27WewmE9hzGVMJkQ"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsTopple = {
+      label: "Excelsior Weapons - Topple",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.RUmcmqMqLrD2JJDK"
+  };
+  
+  CONFIG.DND5E.weaponMasteries.excelsiorWeaponsVex = {
+      label: "Excelsior Weapons - Vex",
+      reference: "Compendium.excelsior-weapons.excelsior-weapons-journals.JournalEntry.9khaQIi08TpEW1cV.JournalEntryPage.Vm5ER7lrZJl18Jg0"
+  };
+
+  CONFIG.DND5E.weaponIds.throwing_knife = "Compendium.excelsior-weapons.excelsior-weapons-equipment.Item.gkBOHXrN29bi1NfK";
+});
